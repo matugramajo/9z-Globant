76afa30e6cd84e7718e4d723d30db1c0b7c5296f
Matilde Gramajo <matildegramajo@hotmail.com>
2024-05-09 15:39:18 -0300

    add footer

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Link from 'next/link';
-import { TeamIcon } from "../9zlogo"
+import Link from "next/link";
+import { TeamIcon } from "../9zlogo";
 
 const Noticias = () => {
   return (
     <>
       <header className="bg-[#0c0c0c] text-white font-mono text-xl tracking-wide">
-      <div className="flex flex-row pb-2">
+        <div className="flex flex-row pb-2">
           <div className="flex items-center justify-center space-x-8 basis-1/3 ">
             <a className="text-2xl font-bold" href="#">
               NOTICIAS
@@ -19,7 +19,7 @@ const Noticias = () => {
           </div>
           <div className="flex items-center justify-center space-x-8 basis-1/3">
             <Link href="/" className="hover:text-lime-400">
-              <TeamIcon/>
+              <TeamIcon />
             </Link>
           </div>
           <div className="flex items-center justify-center space-x-8 basis-1/3">
@@ -33,11 +33,111 @@ const Noticias = () => {
             </Link>
           </div>
         </div>
-      <div style={{ borderBottom: "2px solid #a3e635" }}></div>
+        <div style={{ borderBottom: "2px solid #a3e635" }}></div>
       </header>
-      <main className="bg-[#121212] text-white">
+      <main className="bg-[#121212] text-white"></main>
 
-      </main>
+      <footer className="bg-white dark:bg-[#000000]">
+        <div style={{ borderTop: "2px solid #a3e635" }}></div>
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+          <div className="md:flex md:justify-between">
+            <div className="mb-6 md:mb-0">
+              <a
+                href="https://9z.gg/inicio"
+                className="flex items-center"
+                target="_blank"
+              >
+                <img src="/9z.png" className="h-12 me-15" />
+              </a>
+            </div>
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                  Links
+                </h2>
+                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                  <li className="mb-4">
+                    <a
+                      href="https://shop.9z.gg"
+                      className="hover:text-lime-400"
+                      target="_blank"
+                    >
+                      Tienda
+                    </a>
+                  </li>
+                  <li className="mb-4">
+                    <a href="#">Inicio</a>
+                  </li>
+                  <li className="mb-4">
+                    <Link href="/partidos" className="hover:text-lime-400">
+                      Partidos
+                    </Link>
+                  </li>
+                  <li className="mb-4">
+                    <Link href="/historia" className="hover:text-lime-400">
+                      Historia
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                  Contacto
+                </h2>
+                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                  <li className="mb-4">
+                    <a
+                      href="https://www.instagram.com/9zteam/?hl=es"
+                      className="hover:text-lime-400 "
+                      target="_blank"
+                    >
+                      Instagram
+                    </a>
+                  </li>
+                  <li className="mb-4">
+                    <a
+                      href="https://twitter.com/9zTeam?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+                      className="hover:text-lime-400 "
+                      target="_blank"
+                    >
+                      X
+                    </a>
+                  </li>
+                  <li className="mb-4">
+                    <a
+                      href="https://discord.gg/wdJhqM6D"
+                      className="hover:text-lime-400"
+                      target="_blank"
+                    >
+                      Discord
+                    </a>
+                  </li>
+                  <li className="mb-4">
+                    <a
+                      href="https://www.youtube.com/channel/UCVvzv-yYEnPAYITBfjCriYg"
+                      className="hover:text-lime-400 "
+                      target="_blank"
+                    >
+                      Youtube
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+              <a
+                href="https://twitter.com/_____matilde"
+                className="hover:text-lime-400"
+              >
+                ♡ Matilde Gramajo Dev{" "}
+              </a>
+            </span>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
